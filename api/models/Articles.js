c7936917a460b5babb136755bc613f8f65ae9f0c
Vicky Prahastra <vicky@excide.com>
2019-02-16 14:08:24 +0700

    First commit

---
+++ api/models/Articles.js
@@ -0,0 +1,21 @@
+/**
+ * Articles.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    title: {
+      type: 'string'
+    },
+    body: {
+      type: 'string'
+    }
+  },
+  datastores: 'mongodb'
+
+};
+
