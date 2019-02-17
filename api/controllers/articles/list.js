4eedf61e3419dd2bd959e80b202a5123e1ecd32f
Vicky Prahastra <vickyprahastra@gmail.com>
2019-02-17 12:26:44 +0700

    The default load view from controller

--- api/controllers/articles/list.js
+++ api/controllers/articles/list.js
@@ -1,28 +1,3 @@
-module.exports = {
-
-
-  friendlyName: 'articles page',
-
-
-  description: 'Display the dashboard "Welcome" page.',
-
-
-  exits: {
-
-    success: {
-      viewTemplatePath: 'list',
-      description: 'Display the welcome page for authenticated users.'
-    },
-
-  },
-
-
-  fn: async function (inputs, exits) {
-
-    return exits.success();
-
-  }
-
-
+module.exports =  async function (req, res) {
+  return res.view('list');
 };
-
