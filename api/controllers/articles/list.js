ce9fd882f1167d063a6d8c7a447e80c64651d318
Vicky Prahastra <vickyprahastra@gmail.com>
2019-02-17 12:46:13 +0700

    Refactoring, create articles folder in views

--- api/controllers/articles/list.js
+++ api/controllers/articles/list.js
@@ -1,9 +1,8 @@
 module.exports =  async function (req, res) {
-
   Articles.find({}).exec(function(err, articles) {
     if (err) {
       res.send(500, {error: 'Database Error'});
     }
-    return res.view('list', {articles:articles});
+    return res.view('articles/list', {articles:articles});
   })
 };
